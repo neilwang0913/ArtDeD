4478a91566ee1fdca4ee8e380e41a4754bdb4b1c
Fz Wang <wangfengzhou002@gmail.com>
2022-10-03 20:46:28 +0100

    Update gray_counter.v

--- gray_counter.v
+++ gray_counter.v
@@ -1,3 +1,64 @@
+/*
+
+[https://www.intel.com/content/www/us/en/support/programmable/support-resources/design-examples/horizontal/ver-gray-counter.html]
+
+Verilog Gray Counter Design Example v1.0 README File
+
+This readme file for the Verilog Gray Counter Design contains 
+information about the design example in the Quartus ll HDL 
+Templates. Ensure that you have read the information on the
+Verilog Gray Counter Design Example web page before using the 
+example.
+
+This file contains the following information:
+
+o  Package Contents
+o  Software Tool Requirements
+o  Release History
+o  Technical Support
+
+Package Contents
+================
+
+Verilog Gray Counter Design Example v1.0
+
+
+Software Tool Requirements
+==========================
+
+The Quartus II software version 9.1 or later, 
+or supported versions of third-party synthesis tools. 
+
+Contact your local sales representative if you do not have the
+necessary software tools.
+
+
+Release History
+===============
+
+Version 1.0
+-----------
+- First release of example
+
+
+Technical Support
+=================
+
+Although we have made every effort to ensure that this design
+example works correctly, there might be problems that we have not
+encountered. If you have a question or problem that is not
+answered by the information provided in this readme file or the
+example's documentation, refer to the Altera technical support
+web site: 
+
+http://www.altera.com/mysupport
+
+
+Last updated May, 2010                                
+Copyright (c) 2010 Altera Corporation. All rights reserved.
+
+*/
+	
 // Gray counter
 
 module gray_count
